2eaf00d21202b0c08868e88a4c0444a60d10d565
Naman Khandelwal <khandelwalnaman0101@gmail.com>
2025-06-17 20:48:50 +0530

    fix(cli) : final changes for macOS identifier issue (fix #12674) (#13627)
    * fix(cli) : final changes for macOS identifier issue
    
    * chore:formatted the code

--- crates/tauri-cli/src/build.rs
+++ crates/tauri-cli/src/build.rs
@@ -151,20 +151,11 @@ pub fn setup(
   }
 
   if config_.identifier.ends_with(".app") {
-    if cfg!(target_os = "macos") {
-      log::error!(
-        "The bundle identifier \"{}\" set in `{} identifier` must not end with `.app` on macOS because it conflicts with the application bundle extension.",
-        config_.identifier,
-        bundle_identifier_source
-      );
-      std::process::exit(1);
-    } else {
-      log::warn!(
-        "The bundle identifier \"{}\" set in `{} identifier` ends with `.app`. This is not recommended because it conflicts with the application bundle extension on macOS.",
-        config_.identifier,
-        bundle_identifier_source
-      );
-    }
+    log::warn!(
+      "The bundle identifier \"{}\" set in `{} identifier` ends with `.app`. This is not recommended because it conflicts with the application bundle extension on macOS.",
+      config_.identifier,
+      bundle_identifier_source
+    );
   }
 
   if let Some(before_build) = config_.build.before_build_command.clone() {
